0ee8e34696fbaefe76d890aade945aad435fa06a
janily <nanhuchenjie@163.com>
2020-03-08 14:46:18 +0800

    form validator done

--- js-mini-project/form-validator/app.js
+++ js-mini-project/form-validator/app.js
@@ -17,9 +17,46 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-function checkEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  // return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "邮箱地址不正确");
+  }
+}
+
+function getFieleName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    console.log(input.value);
+    if (input.value.trim() === "") {
+      console.log(input.id);
+      showError(input, `${getFieleName(input)} 不能为空`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieleName(input)} 至少 ${min} 个字符`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieleName(input)} 少于 ${max} 个字符`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordMath(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "密码不匹配");
+  }
 }
 
 //事件
@@ -27,32 +64,9 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
   console.log(username.value);
 
-  if (username.value === " ") {
-    // alert("");
-    showError(username, "请填写用户名");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === " ") {
-    // alert("");
-    showError(email, "请填写邮箱地址");
-  } else if (!checkEmail(email.value)) {
-    showError(email, "请填写正确的邮箱地址");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === " ") {
-    // alert("");
-    showError(password, "请填写密码");
-  } else {
-    showSuccess(password);
-  }
-  if (password2.value === " ") {
-    // alert("");
-    showError(password2, "请确认密码");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMath(password, password2);
 });
