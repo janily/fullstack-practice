4a178cc35186c46a432eeaf3385fa1bb3400efd6
janilychen(陈纬杰) <janilychen@tencent.com>
2020-03-13 15:24:08 +0800

    vue 上传文件实现前端

---
+++ vue-practice/vue-dropfile/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <vue-dropzone ref="dropzone" id="drop1" :options="dropOptions" @vdropzone-complete="afterComplete"></vue-dropzone>
+    <button @click="removeAllFiles">取消上传</button>
+  </div>
+</template>
+<script>
+import vueDropzone from 'vue2-dropzone'
+export default {
+  data: () => ({
+    dropOptions: {
+      url:"localhost:3000/post",
+      maxFilesize: 2, // MB
+      maxFiles: 4,
+      chunking: true,
+      chunkSize: 500, // Bytes
+      thumbnailWidth: 150, // px
+      thumbnailHeight: 150,
+      addRemoveLinks: true
+    }
+  }),
+  components: {
+    vueDropzone
+  },
+  methods: {
+    removeAllFiles() {
+      this.$ref.dropzone.removeAllFiles();
+    },
+    afterComplete(file) {
+      console.log(file);
+    }
+  },
+};
+</script>
