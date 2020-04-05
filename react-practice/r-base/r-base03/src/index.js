24e46ba4a00c174f20d2f3a70ec0fa515b195786
janily <nanhuchenjie@163.com>
2020-04-05 19:29:53 +0800

    react-reduce练习

---
+++ react-practice/r-base/r-base03/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
+
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+//声明一个 App 组件，然后调用 provider 进行包裹。<Provider>是一个提供器，只要使用了这个组件，组件里边的其它所有组件都可以使用store了，这也是React-redux的核心组件了。
+const App = (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
+ReactDOM.render(App, document.getElementById("root"));
