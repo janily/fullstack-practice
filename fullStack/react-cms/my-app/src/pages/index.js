a45bb50d62afe913abd9b97aa3f895eb59ed698d
janilychen <janilychen@tencent.com>
2020-05-13 10:41:40 +0800

    feat: 主页头部UI样式

--- fullStack/react-cms/my-app/src/pages/index.js
+++ fullStack/react-cms/my-app/src/pages/index.js
@@ -2,23 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="Hero">
-      <div className="heroGroup">
+      <div className="HeroGroup">
         <h1>“Learn to <br />design and code React apps”</h1>
         <p>“Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.”</p>
         <p>Now go build something great.</p>
       </div>
     </div>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+    <Link to="/video">Watch the video</Link>
   </Layout>
 )
 
