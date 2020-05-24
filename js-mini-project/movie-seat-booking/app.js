e4f93b2981106649eaa84a2f5317a38aca1be1a8
janily <07jackychen@gmail.com>
2020-05-24 14:15:10 +0800

    :feat:电影选座done

--- js-mini-project/movie-seat-booking/app.js
+++ js-mini-project/movie-seat-booking/app.js
@@ -4,7 +4,15 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+// 保存座位的信息
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
@@ -28,9 +36,31 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// 从 localStorage 获取数据，更新 UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  console.log(selectedSeats);
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
+
 // 更新座位选择信息
 movieSelect.addEventListener('change', e => {
-  ticketPrice = e.target.value
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value)
   updateSelectedCount();
 })
 
@@ -44,3 +74,6 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 })
+
+// 初始化
+updateSelectedCount();
