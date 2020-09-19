7c71423b472f204ce3ac9985a8a9e19028f05f92
janily <07jackychen@gmail.com>
2020-09-19 14:01:41 +0800

    项目初始化

---
+++ react-practice/react-store/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+import App from './components/App'
+import './css/app.scss'
+
+ReactDom.render(<App />, document.getElementById('root'))
+
