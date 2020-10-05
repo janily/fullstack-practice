e7eb79d5a5a22bd02e21757bcb4293ac2e8c2247
janily <07jackychen@gmail.com>
2020-10-05 14:57:56 +0800

    react hooks 练习

---
+++ react-practice/react-hooks/react-hooks-practice/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HookCounterTwo from './components/HookCounterTwo'
+
+function App() {
+  return (
+    <div className="App">
+      <HookCounterTwo />
+    </div>
+  );
+}
+
+export default App;
